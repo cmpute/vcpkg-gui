168f9ed1c23b449d56ef0bd515a441268a914a23
Jacob C <cmpute@qq.com>
2018-02-04 19:30:50 +0800

    Add UI Project

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UserInterface
+namespace Vcpkg
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
