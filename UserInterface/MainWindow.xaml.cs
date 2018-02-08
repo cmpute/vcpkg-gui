1c6c685fc632f875662904a3f9406792b493a0b6
Jacob Zhong <cmpute@qq.com>
2018-02-08 19:14:45 +0800

    Add information tab

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Vcpkg
 {
@@ -22,6 +13,74 @@ namespace Vcpkg
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            ParseVersion();
+        }
+
+        #region Bindings
+
+        public string Version
+        {
+            get { return (string)GetValue(VersionProperty); }
+            set { SetValue(VersionProperty, value); }
+        }
+        public static readonly DependencyProperty VersionProperty =
+            DependencyProperty.Register("Version", typeof(string), typeof(MainWindow), new PropertyMetadata("0.0.0"));
+
+        public string BuildDate
+        {
+            get { return (string)GetValue(BuildDateProperty); }
+            set { SetValue(BuildDateProperty, value); }
+        }
+        public static readonly DependencyProperty BuildDateProperty =
+            DependencyProperty.Register("BuildDate", typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public string BuildHash
+        {
+            get { return (string)GetValue(BuildHashProperty); }
+            set { SetValue(BuildHashProperty, value); }
+        }
+        public static readonly DependencyProperty BuildHashProperty =
+            DependencyProperty.Register("BuildHash", typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        #endregion
+
+        public static int RunVcpkg(string arguments, out string output)
+        {
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = Path.Combine(Properties.Settings.Default.vcpkg_path, "vcpkg.exe"),
+                Arguments = arguments,
+                WorkingDirectory = Properties.Settings.Default.vcpkg_path,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true
+            };
+            var process = Process.Start(info);
+            process.WaitForExit();
+            output = process.StandardOutput.ReadToEnd();
+            return process.ExitCode;
+        }
+
+        public void ParseVersion()
+        {
+            RunVcpkg("version", out string output);
+            var vEnd = output.IndexOf('\n');
+            var vStart = output.LastIndexOf(' ', vEnd);
+            var vstr = output.Substring(vStart, vEnd - vStart).Trim();
+            var splitHead = vstr.IndexOf('-');
+            var splitEnd = vstr.LastIndexOf('-');
+            Version = vstr.Substring(0, splitHead);
+            BuildDate = vstr.Substring(splitHead + 1, splitEnd - splitHead - 1);
+            BuildHash = vstr.Substring(splitEnd + 1);
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            var addr = (sender as Hyperlink).NavigateUri.ToString();
+            if (addr.IndexOf("://") < 0)
+                addr = Path.Combine(Properties.Settings.Default.vcpkg_path, addr);
+            Process.Start(addr);
         }
     }
 }
